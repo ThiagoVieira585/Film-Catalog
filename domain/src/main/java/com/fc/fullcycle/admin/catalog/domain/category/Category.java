73cf86c07ac1d73f59a003b05447438c1f3da356
Thiago Vieira <thiagomlu@hotmail.com.br>
2024-12-19 14:54:11 -0300

    feat: implements cloneable for category

--- domain/src/main/java/com/fc/fullcycle/admin/catalog/domain/category/Category.java
+++ domain/src/main/java/com/fc/fullcycle/admin/catalog/domain/category/Category.java
@@ -5,7 +5,7 @@ import com.fc.fullcycle.admin.catalog.domain.validation.ValidationHandler;
 
 import java.time.Instant;
 
-public class Category extends AggregateRoot<CategoryID> {
+public class Category extends AggregateRoot<CategoryID> implements Cloneable {
     private String name;
     private String description;
     private boolean active;
@@ -92,5 +92,14 @@ public class Category extends AggregateRoot<CategoryID> {
 
     public Instant getDeletedAt() {
         return deletedAt;
+    }
+
+    @Override
+    public Category clone() {
+        try {
+            return  (Category) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
     }
 }
